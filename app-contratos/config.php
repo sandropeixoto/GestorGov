0c447d182fa16b2b112b1c1749fb7387da08e6d7
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-03-16 09:34:00 -0300

    fix(contratos): add SSO_SECRET_KEY to internal config to fix sso access error

--- app-contratos/config.php
+++ app-contratos/config.php
@@ -10,6 +10,9 @@ $user = "eventoss_vocegov";
 $pass = "Senh@2025";
 $db = "eventoss_vocegov";
 
+// Chave Secreta para integração via SSO (Deve ser a mesma do Portal)
+define('SSO_SECRET_KEY', 'GestorGov_Secure_Integration_Token_2026!');
+
 try {
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
     $options = [
